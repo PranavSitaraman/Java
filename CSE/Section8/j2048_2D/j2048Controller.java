8afcab961520959fd726f97afd6c9da12febe5c4
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2022-04-27 21:05:16 -0400

    2d 2048 updated

--- CSE/Section8/j2048_2D/j2048Controller.java
+++ CSE/Section8/j2048_2D/j2048Controller.java
@@ -4,26 +4,15 @@ public class j2048Controller
 {
 	public static void main(String[] args)
 	{
-		Scanner in = new Scanner(System.in);
 		int DIM = 4;
 		j2048Model model = new j2048Model(DIM);
 		j2048View view = new j2048View(DIM);
 		int[][] board = new int[DIM][DIM];
 		model.init(board);
-		System.out.println("Current score: " + model.getScore());
-		view.draw(board);
+		view.draw(board, model.getScore());
 		while (model.hasMove(board))
 		{
-			System.out.print("Move: ");
-			String move = in.nextLine();
-			if (!move.equals("w") && !move.equals("a") && !move.equals("s") && !move.equals("d") && !move.equals("q"))
-			{
-				do
-				{
-					System.out.print("Enter a move: \'w\' for up, \'a\' for left, \'s\' for down, \'d\' for right, and \'q\' to quit: ");
-					move = in.nextLine();
-				} while(!move.equals("w") && !move.equals("a") && !move.equals("s") && !move.equals("d") && !move.equals("q"));
-			}
+			String move = view.getMove();
 			boolean moved = false;
 			if (move.equals("q")) break;
 			else if (move.equals("w")) moved = model.shiftUp(board);
@@ -33,10 +22,9 @@ public class j2048Controller
 			if (moved)
 			{
 				model.spawn(board);
-				System.out.println("Current score: " + model.getScore());
-				view.draw(board);
+				view.draw(board, model.getScore());
 			}
 		}
-		System.out.println("Game over!");
+		view.gameOver(model.getScore());
 	}
 }
--- CSE/Section8/j2048_2D/j2048View.java
+++ CSE/Section8/j2048_2D/j2048View.java
@@ -3,10 +3,16 @@ import java.util.*;
 public class j2048View
 {
 	private int DIM;
-	public j2048View(int DIM) { this.DIM = DIM; }
-	public j2048View() { this(4); }
-	public void draw(int[][] board)
+	Scanner in;
+	public j2048View(int DIM)
 	{
+		this.DIM = DIM;
+		in = new Scanner(System.in);
+	}
+	public j2048View() { this(4); }
+	public void draw(int[][] board, int score)
+	{
+		System.out.println("\nCurrent score: " + score);
 		System.out.println();
 		for (int i = 0; i < DIM; i++)
 		{
@@ -18,5 +24,24 @@ public class j2048View
 			System.out.println();
 		}
 		System.out.println();
+	}
+	public String getMove()
+	{
+		System.out.print("Move: ");
+		String move = in.nextLine();
+		if (!move.equals("w") && !move.equals("a") && !move.equals("s") && !move.equals("d") && !move.equals("q"))
+		{
+			do
+			{
+				System.out.print("Enter a move: \'w\' for up, \'a\' for left, \'s\' for down, \'d\' for right, and \'q\' to quit: ");
+				move = in.nextLine();
+			} while(!move.equals("w") && !move.equals("a") && !move.equals("s") && !move.equals("d") && !move.equals("q"));
+		}
+		return move;
+	}
+	public void gameOver(int score)
+	{
+		System.out.println("\nYour final score is " + score + "!");
+		System.out.println("Game over!");
 	}
 }
